c5198f48b0f98ea3deac39d1be0747d9162376a6
Duilio Palacios <duilio@styde.net>
2022-02-14 20:28:19 +0000

    Add dynamic routes

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,16 @@ Route::get('/notas', function () {
     return 'Listado de notas';
 });
 
+Route::get('notas/{id}', function ($id) {
+    return "Detalle de la nota: {$id}";
+});
+
 Route::get('/notas/crear', function () {
     return 'Crear nueva nota';
+});
+
+Route::get('notas/{id}/editar', function ($id) {
+    return "Editar nota: " . $id;
 });
 
 Route::get('/cursos', function () {
